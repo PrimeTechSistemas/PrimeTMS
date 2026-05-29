95a9495f2749580cadbbf9bdb29cd2de6de15b8d
arthenyo <arthenyocarlos19@gmail.com>
2026-05-29 20:23:33 -0300

    initialize project structure with Maven, Spring Boot, and essential configurations

---
+++ src/main/java/br/com/primetechsistema/primetms/PrimeTmsApplication.java
@@ -0,0 +1,13 @@
+package br.com.primetechsistema.primetms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeTmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeTmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/primetechsistema/primetms/PrimeTmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.primetechsistema.primetms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeTmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
